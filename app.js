11e7595672e9c9b4453f6b9cb760466afdfe950d
VaderSteve76 <alverson.s@yahoo.com>
2020-11-02 15:33:57 -0500

    Save to local storage

--- app.js
+++ app.js
@@ -71,6 +71,18 @@ function filterTodo(e) {
         } else {
           todo.style.display = 'none';
         }
+        break;
     }
   });
+}
+
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
