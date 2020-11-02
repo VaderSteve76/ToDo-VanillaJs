c00b02897218380b6269a73b31b5755a57146922
VaderSteve76 <alverson.s@yahoo.com>
2020-11-02 15:58:09 -0500

    Added remove todos from local storage

--- app.js
+++ app.js
@@ -78,11 +78,23 @@ function filterTodo(e) {
 
 function saveLocalTodos(todo) {
   let todos;
-  if (localStorage.getItem("todos") === null) {
+  if (localStorage.getItem('todos') === null) {
     todos = [];
   } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
+    todos = JSON.parse(localStorage.getItem('todos'));
   }
   todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
