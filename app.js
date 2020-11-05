2ab5f3d4dcae978ce60aae92ff28788bf10c207f
VaderSteve76 <alverson.s@yahoo.com>
2020-11-05 15:48:50 -0500

    Refactor plus added media query for responsive

--- app.js
+++ app.js
@@ -1,54 +1,67 @@
-// Selectors
+//Select DOM
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
 
-// Event Listeners
-todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener('click', filterTodo);
+//Event Listeners
 document.addEventListener('DOMContentLoaded', getTodos);
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteTodo);
+filterOption.addEventListener('click', filterTodo);
 
-// Functions
+//Functions
+
 function addTodo(e) {
-  todoInput.value.length !== 0 ? e.preventDefault() : todoInput.value;
-  // create li
+  //Prevent natural behaviour
+  if(!todoInput.value) {
+    return todoInput.value;
+  } else {
+    e.preventDefault();
+  }
+  //Create todo div
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
+  //Create list
   const newTodo = document.createElement('li');
   newTodo.innerText = todoInput.value;
+  //Save to local - do this last
+  //Save to local
+  saveLocalTodos(todoInput.value);
+  //
   newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
-  // complete
+  todoInput.value = '';
+  //Create Completed Button
   const completedButton = document.createElement('button');
-  completedButton.innerHTML = '<i class="fas fa-check"></i>';
-  completedButton.classList.add('complete-btn');
+  completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+  completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
-  // trash
+  //Create trash button
   const trashButton = document.createElement('button');
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
   trashButton.classList.add('trash-btn');
   todoDiv.appendChild(trashButton);
-  // append to list
+  //attach final Todo
   todoList.appendChild(todoDiv);
-  // clear todoInput value
-  todoInput.value = '';
 }
 
-function deleteCheck(e) {
+function deleteTodo(e) {
   const item = e.target;
-  // delete
-  if(item.classList[0] === 'trash-btn') {
-    item.parentElement.classList.add('fall');
-    // item.parentElement.remove();
-    item.parentElement.addEventListener('transitionend', function() {
-      item.parentElement.remove();
+  if (item.classList[0] === 'trash-btn') {
+    // e.target.parentElement.remove();
+    const todo = item.parentElement;
+    todo.classList.add('fall');
+    //at the end
+    removeLocalTodos(todo);
+    todo.addEventListener('transitionend', e => {
+      todo.remove();
     });
   }
-  // completed
-  if(item.classList[0] === 'complete-btn') {
-    item.parentElement.classList.toggle('completed');
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+    console.log(todo);
   }
 }
 
@@ -72,7 +85,6 @@ function filterTodo(e) {
         } else {
           todo.style.display = 'none';
         }
-        break;
     }
   });
 }
@@ -87,7 +99,6 @@ function saveLocalTodos(todo) {
   todos.push(todo);
   localStorage.setItem('todos', JSON.stringify(todos));
 }
-
 function removeLocalTodos(todo) {
   let todos;
   if (localStorage.getItem('todos') === null) {
@@ -120,7 +131,7 @@ function getTodos() {
     //Create Completed Button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = `<i class="fas fa-check"></i>`;
-    completedButton.classList.add("complete-btn");
+    completedButton.classList.add('complete-btn');
     todoDiv.appendChild(completedButton);
     //Create trash button
     const trashButton = document.createElement('button');
